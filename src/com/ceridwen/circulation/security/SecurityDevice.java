faa876d879a866e2ac79579f1c47f389188d4792
Matthew J. Dovey <m.dovey@ceridwen.com>
2004-10-10 18:08:34 +0000

    Transfer to subversion

---
+++ src/com/ceridwen/circulation/security/SecurityDevice.java
@@ -0,0 +1,39 @@
+/**
+ * <p>Title: Self Issue</p>
+ * <p>Description: Self Issue Client</p>
+ * <p>Copyright: 2004,</p>
+ * <p>Company: ceridwen.com</p>
+ * @author Matthew J. Dovey
+ * @version 2.1
+ */
+
+package com.ceridwen.circulation.security;
+
+public abstract class SecurityDevice {
+  private int timeOut;
+  private int retries;
+  public int getTimeOut() {
+    return timeOut;
+  }
+  public void setTimeOut(int timeOut) {
+    this.timeOut = timeOut;
+  }
+  public int getRetries() {
+    return retries;
+  }
+  public void setRetries(int retries) {
+    this.retries = retries;
+  }
+
+  public abstract void init();
+  public abstract void start(SecurityListener listener);
+  public abstract void stop();
+  public abstract void deinit();
+  public abstract void lock() throws TimeoutException, FailureException;
+  public abstract void unlock() throws TimeoutException, FailureException;
+  public abstract boolean isLocked() throws TimeoutException, FailureException;
+
+  static {
+    com.ceridwen.util.versioning.ComponentRegistry.registerComponent(SecurityDevice.class);
+  }
+}
