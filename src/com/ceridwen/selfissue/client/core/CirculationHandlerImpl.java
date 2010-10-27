20b06cf685ebef7033bd2085f6e83a8bf8971f68
Matthew J. Dovey <m.dovey@ceridwen.com>
2010-10-27 10:55:37 +0000

    Updated copyright notices to GPL3 Added SecurityDevice.num to build scripts Changed failed SecurityDevice to warning and explicit mention defaulting to null device Changed config to load null Security Device

--- src/com/ceridwen/selfissue/client/core/CirculationHandlerImpl.java
+++ src/com/ceridwen/selfissue/client/core/CirculationHandlerImpl.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Matthew J. Dovey.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contributors:
+ *     Matthew J. Dovey - initial API and implementation
+ ******************************************************************************/
 package com.ceridwen.selfissue.client.core;
 
 import java.text.AttributedCharacterIterator;
@@ -89,7 +108,7 @@ public Class<? extends OfflineSpooler> getSpoolerClass() {
       securityDevice = (SecurityDevice) Class.forName(Configuration.getProperty(
           "Systems/Security/@class")).newInstance();
     } catch (Exception ex) {
-      logger.fatal("Could not initialise security device", ex);
+      logger.warn("Could not initialise security device - defaulting to null device", ex);
       securityDevice = new com.ceridwen.selfissue.client.nulldevices.SecurityDevice();
     }
     securityDevice.setRetries(Configuration.getIntProperty(
