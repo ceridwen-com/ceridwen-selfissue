8b1da80f18b1b1d4505bd9940b4374d6d6b8583c
Matthew J. Dovey <m.dovey@ceridwen.com>
2010-11-07 17:07:11 +0000

    Added SyslogLogHandler

---
+++ src/com/ceridwen/selfissue/client/log/SyslogLogger.java
@@ -0,0 +1,33 @@
+package com.ceridwen.selfissue.client.log;
+
+
+import org.w3c.dom.Node;
+
+import com.ceridwen.selfissue.client.config.Configuration;
+import com.ceridwen.selfissue.client.core.OutOfOrderInterface;
+import com.ceridwen.util.net.Syslog;
+
+public class SyslogLogger extends OnlineLogLogger {
+    String host;
+    int port;
+    
+    public void initialise(Node config, OutOfOrderInterface ooo) {
+      super.initialise(config, ooo);
+      host = Configuration.getSubProperty(config, "host");
+      port = Configuration.getIntSubProperty(config, "port");      
+    }
+
+    public synchronized boolean sendSyslogMessage(String facility, int level, String msg) {
+      try {
+          Syslog.sendSyslog(host, port, facility, level , msg);
+          return true;
+      }
+      catch (Exception ex) {
+        return false;
+      }
+    }
+
+    public boolean log(OnlineLogEvent event) {      
+      return this.sendSyslogMessage("SelfIssue", event.isActionRequired()?Syslog.LOG_ALERT:Syslog.LOG_INFO, this.getMessage(event));
+    }
+}
