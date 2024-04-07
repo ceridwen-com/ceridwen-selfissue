6fcb5852c7f22f7b034347423f778b631cdbd6aa
Matthew J. Dovey <m.dovey@ceridwen.com>
2024-04-07 13:38:25 +0100

    Added WinEventLog logger

---
+++ ceridwen-selfissue-client/src/main/java/com/ceridwen/selfissue/client/log/WinEventLogger.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 Ceridwen Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.ceridwen.selfissue.client.log;
+
+import com.ceridwen.selfissue.client.core.OutOfOrderInterface;
+import com.ceridwen.util.logging.WinEventLogHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import org.w3c.dom.Node;
+
+/**
+ *
+ * @author Matthew
+ */
+public class WinEventLogger extends OnlineLogLogger {
+    
+    @Override
+    public void initialise(Node config, OutOfOrderInterface ooo) {
+      super.initialise(config, ooo);
+    }
+
+    public synchronized boolean sendWinEventMessage(String source, Level level, String msg) {
+        WinEventLogHandler handler = new WinEventLogHandler(host, source);
+        handler.setLevel(level);
+        LogRecord record = new LogRecord(level, msg);
+        handler.publish(record);
+        return true;
+    }
+
+    @Override
+    public boolean log(OnlineLogEvent event) { 
+      return this.sendWinEventMessage(source.isBlank()?"Ceridwen SelfIssue Logger":source, event.isActionRequired()?Level.WARNING:Level.INFO, this.getMessage(event));
+    }
+}
